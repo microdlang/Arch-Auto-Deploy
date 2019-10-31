29fac7e788d572f916f0c18d32cf604ddef1e8a7
Ernesto Castellotti <erny.castell@gmail.com>
2019-10-31 18:02:45 +0100

    Remove quotes

--- pkgbuild.sh
+++ pkgbuild.sh
@@ -10,24 +10,24 @@ declare -r pkgrepo="${1#*/}"
 
 # Build packages.
 sudo chown -R pkguser:pkguser packages
-cd "packages"
+cd packages
 
 for dir in */; do
 	cd "$dir"
 	makepkg -Lcrs --noconfirm
-	cp "*.pkg.tar.xz" "../../bin"
-	cd ".."
+	cp *.pkg.tar.xz "../../bin"
+	cd ..
 done
 
 cd ..
 
 # Add package to repository.
-cd "bin"
+cd bin
 
 for pkg in *.pkg.tar.xz; do
 	repo-add "${pkgrepo}.db.tar.gz" "$pkg"
 done
 
-cd ".."
+cd ..
 
 { set +ex; } 2>/dev/null
